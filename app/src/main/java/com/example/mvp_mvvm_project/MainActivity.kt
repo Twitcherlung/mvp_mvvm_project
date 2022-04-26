3b2adc47f63c9d9ce4205c07aa145da93f1b1129
Twitcherlung <wespe96@mail.ru>
2022-04-26 11:06:42 +0300

    Пересоздание проекта для исправления ошибок по сдаче ПЗ

---
+++ app/src/main/java/com/example/mvp_mvvm_project/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.mvp_mvvm_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mvp_mvvm_project.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
